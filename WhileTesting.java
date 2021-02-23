6927aeb87d147f9dd214d01704b4972dc7e98f0a
SamsonTimilehin <79143241+SamsonTimilehin@users.noreply.github.com>
2021-02-23 08:26:07 +0100

    Add files via upload

---
+++ WhileTesting.java
@@ -0,0 +1,21 @@
+
+
+  public class WhileTesting{
+
+ public static void main(String [] args){
+
+  int evenNumbers = 0;
+
+  while(evenNumbers < 100){
+
+  evenNumbers = evenNumbers + 2;
+
+  System.out.println("EVEN NUMBER: " + evenNumbers);
+}
+}
+
+
+
+
+
+}
---
+++ WhileTesting2.java
@@ -0,0 +1,20 @@
+     import java.util.Scanner;
+
+  public class WhileTesting2{
+
+ public static void main(String [] args){
+
+  int oddNumbers = 1;
+
+  while(oddNumbers < 100){
+
+  System.out.println("ODD NUMBER: " + oddNumbers);
+  oddNumbers += 2;
+}
+}
+
+
+
+
+
+}
