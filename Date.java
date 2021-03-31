1b8cc2ef3393dbba0be90f8b712de33a4f3604fd
SamsonTimilehin <79143241+SamsonTimilehin@users.noreply.github.com>
2021-03-31 19:39:45 +0100

    TDD Date Exercise

--- Date.java
+++ Date.java
@@ -1,59 +1,48 @@
-   public class Date{
+package africa.semicolon.mp3;
 
+public class Date {
     private int month;
-
     private int day;
-
     private int year;
 
-  public Date(int month, int day, int year){
 
-   this.month = month;
-   this.day = day;
-   this.year = year;
+    public String displayDate() {
+        String currentDate = month + "/" + day + "/" + year;
+        return currentDate;
+    }
+
+    public void setMonth(int month) {
+        if (month <= 0 || month > 12 ) {
+            throw new IllegalArgumentException("Invalid month! Number out of range");
+        }
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        if(day <= 0 || day > 31) {
+            throw new IllegalArgumentException("Invalid day! Number out of range");
+        }
+        this.day = day;
+    }
+
+    public void setYear(int year) {
+        if(year < 1900 || year > 3001) {
+            throw new IllegalArgumentException("Invalid year! Number out of range");
+        }
+            this.year = year;
+    }
+
+    public int getMonth() {
+
+        return month;
+    }
+
+    public int getDay() {
+
+        return day;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
-
-  public void setMonth(int month){
-   this.month = month; 
-} 
-
-   public void setDay(int day){
-   this.day = day; 
-} 
-
-  public void setYear(int year){
-   this.year = year; 
-}
-
-
-  public int getMonth(){
-    return month;
-}
-
-  public int getDay(){
-    return day;
-}
-
-  public int getYear(){
-    return year;
-}
-
-
- public String displayDate(){
-     String displayDate = month + "/" + day + "/"+ year; 
-     return  displayDate;
-
-}
-
- 
-
-
-}
-
-
-
-
-
-
-
-
--- DateTest.java
+++ DateTest.java
@@ -1,28 +1,76 @@
- public class DateTest{
+package africa.semicolon.mp3;
 
-   
-  public static void main(String [] args){
-  
-  Date dateSetting = new Date( 05, 19, 2020);
-  
-  dateSetting.setMonth(02);
-  dateSetting.setDay(18);
-  dateSetting.setYear(2021);
-  
-  dateSetting.getMonth();
-  dateSetting.getDay();
-  dateSetting.getYear();
-  
-  System.out.printf("TODAY'S DATE %n%s",dateSetting.displayDate());
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-}
+import static org.junit.jupiter.api.Assertions.*;
 
+public class DateTest {
+   private Date date;
+   @BeforeEach
+   void  AllTestStartsWith(){
+       date = new Date();
+   }
+    @Test
+    void dateCanBeCreate(){
+        Date date = new Date();
+        assertNotNull(date);
+    }
+    @Test
+    void dateCanBeSet(){
+        date.setMonth(12);
+        assertEquals(12, date.getMonth());
+        date.setDay(28);
+        assertEquals(28, date.getDay());
+        date.setYear(2000);
+        assertEquals(2000, date.getYear());
+    }
+    @Test
+    void dateCanBeDisplay(){
+        date.setMonth(12);
+        date.setDay(28);
+        date.setYear(2000);
+        assertEquals("12/28/2000", date.displayDate());
+    }
+    @Test
+    void monthCanBeSetWithinOneToTwelve(){
+        date.setMonth(12);
+        date.setMonth(9);
+        date.setDay(28);
+        date.setYear(2000);
+        assertEquals("9/28/2000", date.displayDate());
+    }
+    @Test
+    void dayCanBeSetToNumbersWithinOneToThirtyOne(){
 
-
-
-
-
-
-
-
+        date.setMonth(9);
+        date.setDay(25);
+        date.setYear(2000);
+        assertEquals("9/25/2000", date.displayDate());
+    }
+    @Test
+    void yearCanBeSetToNumberWithin1900To30001(){
+        date.setMonth(9);
+        date.setDay(25);
+        date.setYear(2001);
+        assertEquals("9/25/2001", date.displayDate());
+    }
+    @Test
+     void identifyAndThrowErrorForInvalidMonthDate(){
+        date.setDay(30);
+        date.setYear(2000);
+        assertThrows(IllegalArgumentException.class,() -> date.setMonth(23));
+    }
+    @Test
+    void identifyAndThrowErrorForInvalidDayDate(){
+        date.setMonth(10);
+        date.setYear(3000);
+        assertThrows(IllegalArgumentException.class,() -> date.setDay(40));
+    }
+    @Test
+    void identifyAndThrowErrorForInvalidYearDate(){
+        date.setMonth(11);
+        date.setDay(30);
+        assertThrows(IllegalArgumentException.class,() -> date.setYear(5000));
+    }
 }
