5e940576905aedfc47e915ff72e17c39305dafd9
SamsonTimilehin <79143241+SamsonTimilehin@users.noreply.github.com>
2021-04-14 18:47:28 +0100

    chapter 8

--- Account.java
+++ Account.java
@@ -1,49 +1,32 @@
-  public class Account{
+package africa.semicolon.mp3;
 
-   private String name;
+import java.math.BigDecimal;
 
-   private double balance;
+public class Account {
+    private final String name;
+    private BigDecimal balance;
 
-   public Account(String name, double balance){
+    public Account(String name, BigDecimal balance) {
 
-     this.name = name;
+        this.name = name;
 
-     this.balance = balance; 
+        this.balance = balance;
 
-}
-
-   public void deposit(double depositAmount){
-
-     if(depositAmount > 0.00)
-
-      balance += depositAmount;       
-}
-
-   public void withdrawal(double withdrawAmount){
-
-      if(withdrawAmount > 0.00)	
-
-       balance -= withdrawAmount;
-}
-   public void setName(String name){
-  
-      this.name = name;
-}
-
-  public String getName(){
-
-  return name;
-}
-
-  public double getBalance(){
-
-  return balance;
-
-}
-
-
-
-
+    }
+    public String getName() {
+        return name;
+    }
+    public void deposit(BigDecimal depositAmount) {
+        if (depositAmount.compareTo(BigDecimal.ZERO) > 0)
+        balance = balance.add(depositAmount);
+    }
+    public void withdraw(BigDecimal withdrawAmount) {
+        if (withdrawAmount.compareTo(BigDecimal.ZERO) > 0)
+        balance = balance.subtract(withdrawAmount);
+    }
+    public BigDecimal getBalance() {
+        return balance;
+    }
 
 
 }
--- AccountTest.java
+++ AccountTest.java
@@ -1,38 +1,53 @@
- import java.util.Scanner;
-
- public class AccountTest{
-
- public static void main(String [] args){
-
- Account joyAccount = new Account("Samson", 5000.00);
-
- System.out.println("How much will you like to deposit?");
-
- Scanner input = new Scanner(System.in);
-
-  double depositAmount = input.nextDouble();
-
-  System.out.println("How much will you like to withdraw?");
-
-  double withdrawAmount = input.nextDouble();
-
-  joyAccount.deposit(depositAmount);
- 
-  joyAccount.withdrawal(withdrawAmount);
-
-  System.out.printf("Hello, %s%n",joyAccount.getName());
-  System.out.println("Your account balance is " + joyAccount.getBalance());
+package africa.semicolon.mp3;
 
 
+import org.junit.jupiter.api.Test;
 
-}
+import java.math.BigDecimal;
 
-
-
-
-
+import static org.junit.jupiter.api.Assertions.*;
 
 
+public class AccountTest {
 
+    @Test
+    void customerAccountIsCreated() {
+        Account  account1 = new Account("Jane", new BigDecimal("0.0"));
+        assertNotNull(account1);
+    }
 
+    @Test
+    void accountsCanBeFunded() {
+        Account  account1 = new Account("Jane", new BigDecimal("0.0"));
+        account1.deposit(new BigDecimal("500.0"));
+        assertEquals(new BigDecimal("500.0"), account1.getBalance());
+        assertEquals("Jane", account1.getName());
+    }
+    @Test
+    void customerDepositIsAddedToBalance(){
+        Account  account1 = new Account("Jane", new BigDecimal("0.0"));
+        account1.deposit(new BigDecimal("206.20"));
+        account1.deposit(new BigDecimal("500.17"));
+        assertEquals(new BigDecimal("706.37"), account1.getBalance());
+    }
+    @Test
+    void customerCanNotDepositZeroAmountOrAmountLessThanZero(){
+        Account  account1 = new Account("Jane", new BigDecimal("0.0"));
+        account1.deposit(new BigDecimal("500.0"));
+        account1.deposit(new BigDecimal(-5));
+        assertEquals(new BigDecimal("500.0"), account1.getBalance());
+    }
+    @Test
+    void customerCanWithdrawCash(){
+        Account  account1 = new Account("Jane", new BigDecimal("0.0"));
+        account1.deposit(new BigDecimal("800.10"));
+        account1.withdraw(new BigDecimal("300.24"));
+        assertEquals(new BigDecimal("499.86"), account1.getBalance());
+    }
+    @Test
+    void customerCannotWithdrawNegativeAmount(){
+        Account  account1 = new Account("Jane", new BigDecimal("0.0"));
+        account1.withdraw(new BigDecimal("-1300.0"));
+        assertEquals(new BigDecimal("0.0"), account1.getBalance());
+    }
 }
